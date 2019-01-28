a84e7e20e5cb39fd65179de00967e564bb5e0bb5
bstrong33 <boston.strong33@gmail.com>
2019-01-28 15:49:19 -0700

    working on login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, {component} from 'react';
 import axios from 'axios';
 import { connect } from 'react-redux';
+import { updateUsername, updateId } from './../../ducks/reducer';
 
 class Login extends Component {
     constructor(props) {
@@ -13,10 +14,58 @@ class Login extends Component {
         }
     }
 
+    async login() {
+        let {username, password } = this.state;
+        let res = await axios.post('/auth/login', { username, password });
+        this.setState({
+            username: '', password: '', message: res.data.message
+        })
+        this.props.updateUsername(res.data.username)
+        this.props.updateId(res.data.id)
+        if (res.data.loggedIn) {
+            this.props.history.push('/homepage')
+        }
+    }
+
+    async register() {
+        let { username, password } = this.state
+        let res = await axios.post('/auth/register', { username, password })
+        this.setState({
+            username: '', password: '', message: res.data.message
+        })
+        this.props.updateUsername(res.data.username)
+        this.props.updateId(res.data.id)
+        if (res.data.loggedIn) {
+            this.props.history.push('/homepage')
+        }
+    }
+
     render() {
         return (
-            <div>
-
+            <div className='login-background'>
+                <div className='login'>
+                    <h3>Happy Gardening!</h3>
+                    <div className='username'>
+                        <p>Username:
+                        <input
+                                value={this.state.username}
+                                onChange={e => this.setState({ username: e.target.value })}
+                            />
+                        </p>
+                    </div>
+                    <div className='password'>
+                        <p>Password:
+                        <input
+                                value={this.state.password}
+                                onChange={e => this.setState({ password: e.target.value })}
+                                type="password"
+                            />
+                        </p>
+                    </div>
+                    <button onClick={() => this.login()}>Login</button>
+                    <button onClick={() => this.register()}>Register</button>
+                    <p className='login-error'>{this.state.message}</p>
+                </div>
             </div>
         )
     }
