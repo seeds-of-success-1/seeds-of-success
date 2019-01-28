6e00f03a914352c0ec6be2b2d69837669fb06f8d
DougCaswell <dougcaswell@gmail.com>
2019-01-28 16:00:07 -0700

    got simple testing done

--- src/logic/logic.test.js
+++ src/logic/logic.test.js
@@ -2,18 +2,18 @@ import { toggle } from './logic';
 
 describe('Tests Toggle Show button', () => {
     test('when given true, returns false', () => {
-      expect(toggle(true)).toBe(false)
+        expect(toggle(true)).toBe(false)
     })
-  
+
     test('when given false, returns true', () => {
-      expect(toggle(false)).toBe(true)
+        expect(toggle(false)).toBe(true)
     })
-  
+
     test('if given an object, should return false', () => {
-      expect(toggle({})).toBe(false)
+        expect(toggle({})).toBe(false)
     })
-  
+
     test('if given a 0, should return true', () => {
-      expect(toggle(0)).toBe(true)
+        expect(toggle(0)).toBe(true)
     })
-  });
+});
