50388ede104aa90fea7840d0ae74373ffa9bc6cc
Craig Stevens <csteves44@gmail.com>
2019-01-29 16:33:29 -0700

    Add image assets

--- src/services/plants.js
+++ src/services/plants.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import {getPlantKey} from '../config';
 
 export const getPlants = function(){
-    console.log('hello from getPlants')
     return axios.get(`http://harvesthelper.herokuapp.com/api/v1/plants?api_key=${getPlantKey()}`)
     .then(res => res.data);
 }
